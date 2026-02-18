2a21ebecef42e662916a1e33a5756a89b41cbace
shardul-011 <shardul1913042@akgec.ac.in>
2026-02-18 11:12:18 +0530

    Initial commit

---
+++ get_lib_versions.py
@@ -0,0 +1,15 @@
+import importlib.metadata
+packages=[
+    "rages",
+    "langchain--mcp-adapters",
+    "mcp",
+    "ddgs",
+    "langchain-openai"
+]
+
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}: {version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg}: Not installed")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from smartdoc!")
+
+
+if __name__ == "__main__":
+    main()
